6b1b373f5eef3143844a459f1390676d2d8edb4e
cykris1822125 <76935565+cykris1822125@users.noreply.github.com>
2021-01-30 19:57:46 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+  <body>
+<bgcolor> = Yellow
+    <font face=Adobe Gothic Std"font size="16"font color="Sky blue"><b><marquee>TODAY:</marquee></b></font>
+      <html>
+  <body>
+   How covid 19 Affect us<br> <br>
+Many people loss their job, main income, affect businesses<br> and even suffer on their own finances. Affects people's lives physically,<br> mentally and spiritually as well. When we take a look at on <br> it's bright side we get to know ourself here, with <br> a span of time we focus on how to grow our self confidence, and <br> even let us realize a lot of things like how to handle <br> our finances. And reflect to our own doings. Covid-19 has advantage <br> and disadvantages in some ways, is that how people deal and react to it.  
+  </body>  
+</html>
+  </body>  
+</html>
